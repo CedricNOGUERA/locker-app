2649fe21983cfdeedf90b0a2e6ebcc0a9b3f5bf7
CedricNOGUERA <cedric.noguera@lwane.com>
2023-04-19 16:07:27 -1000

    auth form design

--- src/Screens/Auth/Auth.js
+++ src/Screens/Auth/Auth.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react'
 import {
   Image,
   Keyboard,
@@ -11,61 +11,60 @@ import {
   TextInput,
   TouchableOpacity,
   ScrollView,
-} from "react-native";
-import IconMCI from "react-native-vector-icons/MaterialCommunityIcons";
+} from 'react-native'
+import IconMCI from 'react-native-vector-icons/MaterialCommunityIcons'
+import IconAntd from 'react-native-vector-icons/AntDesign'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 import { AsyncStorage } from 'react-native'
 
 const Auth = ({ navigation, AppStates }) => {
+  const { setFilteredUserData, filteredUserData } = AppStates
 
-  const {setFilteredUserData, filteredUserData} = AppStates
-
-  const { isLogged, setIsLogged } = AppStates;
-  const [usersData, setUsersData] = useState([]);
-  const [email, setEmail] = useState("");
-  const [pass, setPass] = useState("");
-  const [isError, setIsError] = useState(false);
+  const { isLogged, setIsLogged } = AppStates
+  const [usersData, setUsersData] = useState([])
+  const [email, setEmail] = useState('')
+  const [pass, setPass] = useState('')
+  const [isError, setIsError] = useState(false)
+  const [isVisible, setIsVisible] = useState(false)
 
   useEffect(() => {
-    getAllUsers();
-  }, []);
+    getAllUsers()
+  }, [])
 
   const getAllUsers = () => {
-    var myHeaders = new Headers();
+    var myHeaders = new Headers()
     myHeaders.append(
-      "apikey",
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjaHJ2cW12Y2lhZ3RybWZscG9lIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njk2NjM4OTcsImV4cCI6MTk4NTIzOTg5N30.P0I1e92t1SE51o-8sqS2iCPpP1TkJljtDnP-1aA3dKQ"
-    );
+      'apikey',
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjaHJ2cW12Y2lhZ3RybWZscG9lIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njk2NjM4OTcsImV4cCI6MTk4NTIzOTg5N30.P0I1e92t1SE51o-8sqS2iCPpP1TkJljtDnP-1aA3dKQ'
+    )
 
     var requestOptions = {
-      method: "GET",
+      method: 'GET',
       headers: myHeaders,
-      redirect: "follow",
-    };
+      redirect: 'follow',
+    }
 
-    fetch(
-      "https://fchrvqmvciagtrmflpoe.supabase.co/rest/v1/users?select=*",
-      requestOptions
-    )
+    fetch('https://fchrvqmvciagtrmflpoe.supabase.co/rest/v1/users?select=*', requestOptions)
       .then((response) => response.json())
       .then((result) => {
-        setUsersData(result);
+        setUsersData(result)
       })
-      .catch((error) => console.log("error", error));
-  };
+      .catch((error) => console.log('error', error))
+  }
 
   const onChangeEmail = (text) => {
-    setEmail(text);
-  };
+    setEmail(text)
+  }
   const onChangePass = (text) => {
-    setPass(text);
-  };
+    setPass(text)
+  }
 
   async function storeData(value) {
     try {
-      await AsyncStorage.setItem('key', value);
-      console.log("object")
+      await AsyncStorage.setItem('key', value)
+      console.log('object')
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
   }
 
@@ -73,69 +72,99 @@ const Auth = ({ navigation, AppStates }) => {
     if (Array.isArray(usersData)) {
       const filterUser = usersData?.filter(
         (user) => user?.username === email && user?.pass === pass
-      );
+      )
 
       if (filterUser && filterUser.length > 0) {
-        setIsLogged(true);
-        setIsError(false);
+        setIsLogged(true)
+        setIsError(false)
         setFilteredUserData(filterUser)
         storeData(filterUser)
-        setEmail("");
-        setPass("");
-        navigation.navigate("Home");
+        setEmail('')
+        setPass('')
+        navigation.navigate('Home')
       } else {
-        setIsError(true);
-        setIsLogged(false);
+        setIsError(true)
+        setIsLogged(false)
       }
     } else {
-      alert("c la hesss : " + usersData.id);
+      alert('c la hesss : ' + usersData.id)
     }
-  };
+  }
 
   return (
     <KeyboardAvoidingView
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       style={styles.screen}
     >
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <ScrollView style={{ width: 500, height: "100%" }}>
+        <ScrollView style={{ width: 500, height: '100%' }}>
           <View style={styles.screen}>
             <View style={styles.container}>
               <View
                 style={{
                   flex: 1,
-                  justifyContent: "center",
-                  alignItems: "center",
+                  justifyContent: 'center',
+                  alignItems: 'center',
                 }}
               >
-                <IconMCI name="truck-fast-outline" size={120} color="#00caed" />
+                <IconMCI name='truck-fast-outline' size={120} color='#fff' />
                 <Text
                   style={{
-                    textAlign: "center",
+                    textAlign: 'center',
                     fontSize: 50,
-                    fontWeight: "bold",
+                    fontWeight: 'bold',
+                    color: '#fff'
                   }}
                 >
                   Locker
                 </Text>
               </View>
-              <View style={{ flex: 2, width: 250 }}>
-                <TextInput
-                  style={styles.input}
-                  onChangeText={onChangeEmail}
-                  value={email}
-                  placeholder="Saisissez votre email"
-                  keyboardType="ascii-capable"
-                />
-                <TextInput
-                  style={styles.input}
-                  onChangeText={onChangePass}
-                  value={pass}
-                  placeholder="Saisissez votre mot de passe"
-                  keyboardType="ascii-capable"
-                />
+              <View style={{ flex: 2, width: 300, marginTop: 25}}>
+                <View style={{ flexDirection: 'row', marginBottom: 15 }}>
+                  <View
+                    style={{
+                      backgroundColor: '#b6b6b6',
+                      height: 40,
+                      padding: 5,
+                    }}
+                  >
+                    <IconAntd name='user' size={28} color='#fff' />
+                  </View>
+                  <TextInput
+                    style={styles.input}
+                    onChangeText={onChangeEmail}
+                    value={email}
+                    placeholder='Pseudo'
+                    keyboardType='ascii-capable'
+                  />
+                </View>
+                <View style={{flexDirection: 'row', marginBottom: 15 }}>
+                  <View
+                    style={{
+                      backgroundColor: '#b6b6b6',
+                      height: 40,
+                      padding: 5,
+                    }}
+                  >
+                    <IconAntd name='lock1' size={28} color='#fff' />
+                  </View>
+                  <TextInput
+                    style={styles.input}
+                    onChangeText={onChangePass}
+                    value={pass}
+                    placeholder='Mot de passe'
+                    keyboardType='ascii-capable'
+                    secureTextEntry={isVisible}
+                  />
+                    <Ionicons name={isVisible ? 'eye-sharp' : 'eye-off-sharp'} size={28} color='#b6b6b6' style={{position: "absolute", 
+                    right: 0, top: 5
+                     }}
+                     onPress={() => setIsVisible(!isVisible)}
+                      />
+                </View>
+              
                 {isError && (
-                  <Text style={{ color: "#f00", width: 280 }}>
+                  <Text style={{ color: '#f00', width: 280 }}>
                     Vos données sont erronées, réessayez !!!
                   </Text>
                 )}
@@ -144,16 +173,14 @@ const Auth = ({ navigation, AppStates }) => {
                     height: 40,
                     marginTop: 20,
                     marginBottom: 20,
-                    backgroundColor: "#00caed",
+                    backgroundColor: '#00caed',
                     // backgroundColor: "#505050",
                     padding: 10,
                     borderRadius: 20,
                   }}
                   onPress={authentification}
                 >
-                  <Text style={{ textAlign: "center", color: "#fff" }}>
-                    Valider
-                  </Text>
+                  <Text style={{ textAlign: 'center', color: '#fff' }}>Valider</Text>
                 </TouchableOpacity>
               </View>
             </View>
@@ -161,33 +188,30 @@ const Auth = ({ navigation, AppStates }) => {
         </ScrollView>
       </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   screen: {
-    marginTop: Platform.OS === "ios" ? "10%" : "20%",
-    justifyContent: "center",
-    alignItems: "center",
+    marginTop: Platform.OS === 'ios' ? '10%' : '20%',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   container: {
     // flex: 0.6,
-    width: "80%",
-    backgroundColor: "#fff",
+    width: '80%',
+    backgroundColor: '#a5a5a5',
     padding: 20,
     borderRadius: 10,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   input: {
     height: 40,
-    width: 230,
-    margin: 12,
-    marginTop: 20,
-    borderWidth: 1,
-    borderRadius: 10,
+    width: 280,
+    backgroundColor: '#fff',
     padding: 10,
   },
-});
+})
 
-export default Auth;
+export default Auth
